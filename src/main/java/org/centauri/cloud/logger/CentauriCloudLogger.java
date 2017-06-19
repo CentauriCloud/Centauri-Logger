9814f932af93ffc0f989610eb72ca07d7d2f3a57
jsauvain <joel.sauvain@hotmail.com>
2017-06-19 13:52:52 +0200

    directories changed

--- src/main/java/org/centauri/cloud/logger/CentauriCloudLogger.java
+++ src/main/java/org/centauri/cloud/logger/CentauriCloudLogger.java
@@ -1,13 +1,11 @@
 package org.centauri.cloud.logger;
 
 import lombok.Getter;
-import lombok.extern.log4j.Log4j2;
 import org.centauri.cloud.cloud.Cloud;
-import org.centauri.cloud.cloud.plugin.AbstractModule;
+import org.centauri.cloud.cloud.module.AbstractModule;
 import org.centauri.cloud.logger.config.Config;
 import org.centauri.cloud.logger.listener.EventListener;
 
-@Log4j2
 public class CentauriCloudLogger extends AbstractModule {
 	
 	@Getter private static CentauriCloudLogger instance;
@@ -22,12 +20,12 @@ public class CentauriCloudLogger extends AbstractModule {
 			this.config.loadDefaults();
 			this.config.loadValues();
 		} catch (Exception ex) {
-			this.log.catching(ex);
+			Cloud.getLogger().error("Exception", ex);
 		}
 		
 		Cloud.getInstance().getEventManager().registerEventHandler(new EventListener());
 
-		this.log.info("CentauriCloudLogger enabled!");
+		Cloud.getLogger().info("CentauriCloudLogger enabled!");
 	}
 	
 	@Override
